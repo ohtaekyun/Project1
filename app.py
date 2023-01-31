4f9e12f58823ebacaf5c39886c33688a2b4b5883
ohtaekyun <ohtaekyun@naver.com>
2023-01-31 13:45:02 +0900

    updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib as mpl
 import seaborn as sns
 # import plotly
-import sklearn
+# import sklearn
 import numpy as np
 
 def main():
@@ -12,7 +12,7 @@ def main():
     st.title("matplotlib 버전: " + str(mpl.__version__))
     st.title("seaborn 버전: " + str(sns.__version__))
     # st.title("plotly 버전: " + str(plotly.__version__))
-    st.title("sklearn 버전: " + str(sklearn.__version__))
+    # st.title("sklearn 버전: " + str(sklearn.__version__))
     st.title("numpy 버전: " + str(np.__version__))
 
 if __name__ == "__main__":

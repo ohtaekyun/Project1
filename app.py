266cc4c021b3a4d0b11fe875b493a7ea1bf2c58f
ohtaekyun <ohtaekyun@naver.com>
2023-01-31 12:26:23 +0900

    updated

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import matplotlib as mpl
+import numpy as np
+
+def main():
+    st.title("streamlit 버전: " + str(st.__version__))
+
+if __name__ == "__main__":
+    main()

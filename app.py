39461827e9afb96a08fbce7aff2d703421c11aed
ohtaekyun <ohtaekyun@naver.com>
2023-01-31 13:39:10 +0900

    updated

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib as mpl
 import seaborn as sns
-import plotly as plot
-import sklearn as sk
+import plotly
+import sklearn
 import numpy as np
 
 def main():
@@ -11,8 +11,8 @@ def main():
     st.title("pandas 버전: " + str(pd.__version__))
     st.title("matplotlib 버전: " + str(mpl.__version__))
     st.title("seaborn 버전: " + str(sns.__version__))
-    st.title("plotly 버전: " + str(plot.__version__))
-    st.title("sklearn 버전: " + str(sk.__version__))
+    st.title("plotly 버전: " + str(plotly.__version__))
+    st.title("sklearn 버전: " + str(sklearn.__version__))
     st.title("numpy 버전: " + str(np.__version__))
 
 if __name__ == "__main__":
